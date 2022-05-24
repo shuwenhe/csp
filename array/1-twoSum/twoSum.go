610673b342ef1788ed5e1919f71a960109a073d4
ShuwenHe <1201220707@pku.edu.cn>
2022-05-24 20:43:25 +0800

    feat: twoSum

--- array/1-twoSum/twoSum.go
+++ array/1-twoSum/twoSum.go
@@ -11,5 +11,18 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("twoSum")
+	nums := []int{2, 7, 11, 15}
+	target := 26
+	fmt.Println(twoSum(nums, target))
+}
+
+func twoSum(nums []int, target int) []int {
+	for i, v := range nums {
+		for j := i + 1; j < len(nums); j++ {
+			if v+nums[j] == target {
+				return []int{i, j}
+			}
+		}
+	}
+	return nil
 }
