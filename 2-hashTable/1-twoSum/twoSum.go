4a61e6a1dec3059fc174148bbfaf7abae9cd73e5
ShuwenHe <1201220707@pku.edu.cn>
2022-05-24 22:02:51 +0800

    fix: array & hash table

--- 2-hashTable/1-twoSum/twoSum.go
+++ 2-hashTable/1-twoSum/twoSum.go
@@ -18,7 +18,6 @@
 // -10^9 <= nums[i] <= 10^9
 // -10^9 <= target <= 10^9
 // Only one valid answer exists.
-
 // Follow-up: Can you come up with an algorithm that is less than O(n2) time complexity?
 package main
 
