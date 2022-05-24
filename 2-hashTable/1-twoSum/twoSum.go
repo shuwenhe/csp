4689f0920a381ab2f9801305a3fb55d326f6c228
ShuwenHe <1201220707@pku.edu.cn>
2022-05-24 21:13:32 +0800

    feat: two sum

--- 2-hashTable/1-twoSum/twoSum.go
+++ 2-hashTable/1-twoSum/twoSum.go
@@ -17,4 +17,14 @@ func main() {
 }
 
 func twoSum(nums []int, target int) []int {
+	m := make(map[int]int)
+	for i,v := range nums{
+		j,ok := m[i]
+		if ok{
+			return []int{j,i}
+		}else{
+			m[target-v] = j
+		}
+	}
+	return nil
 }
